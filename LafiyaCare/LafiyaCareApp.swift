68c3054e4950649ab93026144093199ce5552922
Khaled Mohamed Ali <mohamedk812.ka@gmail.com>
2025-05-16 13:37:03 -0400

    renamed App

---
+++ LafiyaCare/LafiyaCareApp.swift
@@ -0,0 +1,30 @@
+//
+//  LafiyaCare_IOSApp.swift
+//  LafiyaCare-IOS
+//
+//  Created by Khaled Mohamed Ali on 5/16/25.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct LafiyaCare_IOSApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        print("Firebase Configured")
+        return true
+    }
+}
