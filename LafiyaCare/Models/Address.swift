afcab8a8927ce0f9948e417b2b1c50ae27225d42
Khaled Mohamed Ali <mohamedk812.ka@gmail.com>
2025-05-16 13:36:28 -0400

    Created Pharmacy Model

---
+++ LafiyaCare/Models/Address.swift
@@ -0,0 +1,12 @@
+//
+//  Address.swift
+//  LafiyaCare-IOS
+//
+//  Created by Khaled Mohamed Ali on 5/16/25.
+//
+
+import Foundation
+class Address: Codable {
+    var latitude: Double
+    var longitude: Double
+}
---
+++ LafiyaCare/Models/Pharmacy.swift
@@ -0,0 +1,25 @@
+//
+//  Pharmacy.swift
+//  LafiyaCare-IOS
+//
+//  Created by Khaled Mohamed Ali on 5/16/25.
+//
+
+import Foundation
+class Pharmacy: Codable{
+    /*
+     isEmergency
+     name
+     openHours
+     phone
+     placeId
+     rating     */
+    var address:Address?
+    var isEmergency:Bool?
+    var name:String?
+    var openHours:String?
+    var placeId:String?
+    var phone:String?
+    var rating:Double?
+    
+}
