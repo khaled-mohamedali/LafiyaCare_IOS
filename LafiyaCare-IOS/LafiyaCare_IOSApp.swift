adf18a5ccf0f50420c3cc70fe423bc862f0ef2a8
Khaled Mohamed Ali <mohamedk812.ka@gmail.com>
2025-05-16 09:43:56 -0400

    Initial Commit

---
+++ LafiyaCare-IOS/LafiyaCare_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LafiyaCare_IOSApp.swift
+//  LafiyaCare-IOS
+//
+//  Created by Khaled Mohamed Ali on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct LafiyaCare_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
