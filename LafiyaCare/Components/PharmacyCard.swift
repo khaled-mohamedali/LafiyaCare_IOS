00567623e5ffb6e9af791f5be6670275d3a13072
Khaled Mohamed Ali <mohamedk812.ka@gmail.com>
2025-05-17 16:56:49 -0400

    refractor pharmacy class

---
+++ LafiyaCare/Components/PharmacyCard.swift
@@ -0,0 +1,18 @@
+//
+//  PharmacyCard.swift
+//  LafiyaCare
+//
+//  Created by Khaled Mohamed Ali on 5/16/25.
+//
+
+import SwiftUI
+
+struct PharmacyCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PharmacyCard()
+}
