56a4d8a0ccb7e52478d58aba72c4d002bef9c75b
Khaled Mohamed Ali <mohamedk812.ka@gmail.com>
2025-05-21 12:38:14 -0400

    Created Emergency Pharmacy Model

--- LafiyaCare/Models/EmergencyPharmacy.swift
+++ LafiyaCare/Models/EmergencyPharmacy.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+class EmergencyPharmacy {
+    var name: String
+    var id: String
+    
+    init(name: String, id: String) {
+        self.name = name
+        self.id = id
+    }
+}
